0790162c560f075dfcfbded886f0dcb87d609295
Jalalflkm <mhmdjalal2103@gmail.com>
2025-11-10 05:17:04 +0700

    Create variabel.java

---
+++ variabel.java
@@ -0,0 +1,22 @@
+public class Variabel{
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String namaLengkap = "Jalal";
+        String tempatTanggalLahir = "Bogor, 24 Mei 2005";
+        String jenisKelamin = "Laki-Laki";
+        String alamatLengkap = "KP. Batukasur Rt 01 Rw 03 Desa Batulayang Kecamatan Cisarua";
+        String noHp = "088289351859";
+        String penghasilanSebulan = "Rp 5.000.000";
+
+        // Menampilkan output
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap          : " + namaLengkap);
+        System.out.println("Tempat & Tanggal Lahir: " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin         : " + jenisKelamin);
+        System.out.println("Alamat Lengkap        : " + alamatLengkap);
+        System.out.println("No Hp                 : " + noHp);
+        System.out.println("Penghasilan Sebulan   : " + penghasilanSebulan);
+    }
+}
